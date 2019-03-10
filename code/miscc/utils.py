8fcc81af6c02468d803740e9b550a9ac123bd8b8
EC2 Default User <ec2-user@ip-172-31-30-44.us-east-2.compute.internal>
2019-03-10 21:23:54 +0000

    added default pillow font

--- code/miscc/utils.py
+++ code/miscc/utils.py
@@ -36,7 +36,6 @@ def drawCaption(convas, captions, ixtoword, vis_size, off1=2, off2=2):
     fnt = ImageFont.truetype('Pillow/Tests/fonts/FreeMono.ttf', 50)
     fnt = ImageFont.load_default()
     
-    
     # get a drawing context
     d = ImageDraw.Draw(img_txt)
     sentence_list = []
