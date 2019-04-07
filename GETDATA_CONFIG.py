989177f36047f029ef8bd8f83a8652ff2fd24422
gryBox <livni.itay@gmail.com>
2019-04-07 03:43:03 +0000

    Added DAMSM photosynthesis directory - elimanted others

---
+++ GETDATA_CONFIG.py
@@ -0,0 +1,15 @@
+
+
+# TODO replace hard code data directories..
+
+# Config to download google docs using the package 
+img_args = {"keywords":"photosynthesis",
+             "format": "png",
+              "limit": numText_files,
+             "output_directory": 'data',
+            "metadata": True,
+            "image_directory": "photosynthesis/images",
+            "no_download": False,
+            "extract_metadata":True
+            # "size":"icon"
+           }
